28e8161882c3ac1739834eca9dfc69935558e3a3
marwa <alqtta04@gmail.com>
2025-04-03 23:21:57 +0200

    add Clean_Architecture

---
+++ e-commerce.Domain/Entites/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce.Infrastructure.Entites;
+
+[Table("Seller")]
+public partial class Seller
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [InverseProperty("Seller")]
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
